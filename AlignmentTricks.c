0fc565cf7cc555bb6404e90508f02c475788092a
kokoronomagnet <antonshreyner@gmail.com>
2016-10-25 14:18:35 +0700

    struct to char array

--- AlignmentTricks.c
+++ AlignmentTricks.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 char char_array[ 10 ] = { 'A' , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 };
+char char_array1[ 10 ] = { 'A' , 0 , 0 , 0 , 0x01 , 0xff , 0xff , 0xff , 0 , 0 };
 char char_array2[ 10 ] = { 0x01 , 0xff , 0xff , 0xff , 'B' , 0 , 0 , 0 , 0 , 0 };
 typedef struct
 {
@@ -13,7 +14,11 @@ typedef struct
 } Test2;
 int main( int argc , char **argv )
 {
-	Test *t = ( Test* )( void * )char_array;
+	Test *t = ( Test* )( void* )0;
+	printf( "%i %i\n" , &t->a , &t->b );
+	t = ( Test* )( void * )char_array;
+	printf( "%c %i\n" , t->a , t->b );
+	t = ( Test* )( void * )char_array1;
 	printf( "%c %i\n" , t->a , t->b );
 	Test2 *t2 = ( Test2* )( void * )char_array2;
 	printf( "%c %i\n" , t2->a , t2->b );
