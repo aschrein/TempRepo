3146310fca947635453d674acd853e9bb5d880a8
kokoronomagnet <antonshreyner@gmail.com>
2016-10-26 12:59:21 +0700

    complex numbers

--- Complex.c
+++ Complex.c
@@ -81,6 +81,7 @@ int main( int argc , char **argv )
 	) );//2.000000 * e^i0.000000
 	printComplex( divr( ( Complex ){ 1.0f , 1.0f } , 0 ) );//{ r: NAN i: NAN }
 	printComplex( mul( ( Complex ){ 1.0f , 1.0f } , ( Complex ){ 1.0f , -1.0f } ) );//{ r: 2.000000 i: 0.000000 }
+	printComplex( div( ( Complex ){ 1.0f , 1.0f } , ( Complex ){ 1.0f , -1.0f } ) );//{ r: 0.000000 i: 1.000000 }
 	printComplex( sub( ( Complex ){ 1.0f , 1.0f } , ( Complex ){ 1.0f , -1.0f } ) );//{ r: 0.000000 i: 2.000000 }
 	printComplex( add( ( Complex ){ 1.0f , 1.0f } , ( Complex ){ 1.0f , -1.0f } ) );//{ r: 2.000000 i: 0.000000 }
 	printf( "%f\n" , mod( ( Complex ){ 3.0f , 4.0f } ) );//5
