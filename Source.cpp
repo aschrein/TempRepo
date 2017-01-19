38df09174989303b8fd6b5c6be4792f243003247
anton shreyner <antonshreyner@gmail.com>
2017-01-19 16:35:31 +0700

    fix

--- Source.cpp
+++ Source.cpp
@@ -3,9 +3,9 @@
 #include <limits>
 namespace SchreinerA
 {
-	static constexpr auto EPS = std::numeric_limits<double>::epsilon();
-	static constexpr auto MIN = std::numeric_limits<double>::min();
-	static constexpr auto DELTA = 1.0;
+	static const double EPS = std::numeric_limits<double>::epsilon();
+	//static const double MIN = std::numeric_limits<double>::min();
+	//static const double DELTA = 1.0;
 	struct vec3
 	{
 		double x , y , z;
@@ -67,21 +67,21 @@ namespace SchreinerA
 	};
 	struct plane
 	{
-		vec3 o , n;
+		vec3 origin , normal;
 		bool contains( vec3 const &p ) const
 		{
-			return abs( ( p - o ) * n ) / ( 1.0 + p.mod() ) / EPS < 10.0;
+			return abs( ( p - origin ) * normal ) / ( 1.0 + p.mod() ) / EPS < 10.0;
 		}
 		vec3 project( vec3 const &p ) const
 		{
-			return p - n * ( n * ( p - o ) );
+			return p - normal * ( normal * ( p - origin ) );
 		}
 		vec3 project( vec3 const &a , vec3 const &b ) const
 		{
 			vec3 v = ( b - a ).norm();
-			double prj = -n * ( a - o );
-			double dot = v * n;
-			if( abs( dot / prj ) > EPS * 10.0 )
+			double prj = -normal * ( a - origin );
+			double dot = v * normal;
+			if( abs( dot ) > 0.0 )
 			{
 				double t = prj / dot;
 				if( t <= 0.0 )
@@ -112,46 +112,46 @@ namespace SchreinerA
 				triangle{ c , a , p }.area();
 			double ar1 = area();
 			return
-				abs( ar0 / ar1 - 1.0 ) / EPS < 10.0;
+				abs( ar0 / ar1 - 1.0 ) / EPS < 100.0;
 		}
 	};
 	struct edge
 	{
-		vec3 o , e;
+		vec3 origin , end;
 		bool intersects( vec3 const &a ) const
 		{
-			if( a == o || a == e )
+			if( a == origin || a == end )
 			{
 				return true;
 			}
-			if( o == e )
+			if( origin == end )
 			{
 				return false;
 			}
 			return abs(
-				( ( a - e ).mod() +
-				( a - o ).mod() ) /
-				( o - e ).mod() - 1.0 ) / EPS < 10.0;
+				( ( a - end ).mod() +
+				( a - origin ).mod() ) /
+				( origin - end ).mod() - 1.0 ) / EPS < 10.0;
 		}
 		bool intersects( vec3 const &a , vec3 const &b ) const//no degenerate cases
 		{
 			if(
-				a == o ||
-				a == e ||
-				b == o ||
-				b == e
+				a == origin ||
+				a == end ||
+				b == origin ||
+				b == end
 				)
 			{
 				return true;
 			}
-			plane p{ a , ( ( b - a ) ^ ( o - a ) ).norm() };
-			if( p.contains( e ) )
+			plane p{ a , ( ( b - a ) ^ ( origin - a ) ).norm() };
+			if( p.contains( end ) )
 			{
-				vec3 v0 = ( e - o ).norm();
+				vec3 v0 = ( end - origin ).norm();
 				vec3 v1 = ( b - a ).norm();
 				double det = v0.x * v1.y - v1.x * v0.y;
-				vec3 dp = a - o;
-				double mod0 = ( e - o ).mod();
+				vec3 dp = a - origin;
+				double mod0 = ( end - origin ).mod();
 				double mod1 = ( a - b ).mod();
 				if( abs( det ) > EPS )
 				{
@@ -179,8 +179,8 @@ namespace SchreinerA
 		bool intersects( vec3 const &a , vec3 const &b , vec3 const &c ) const
 		{
 			if(
-				!e.isFinite() ||
-				!o.isFinite() ||
+				!end.isFinite() ||
+				!origin.isFinite() ||
 				!a.isFinite() ||
 				!b.isFinite() ||
 				!c.isFinite()
@@ -188,31 +188,31 @@ namespace SchreinerA
 			{
 				return false;
 			}
-			if( triangle{ a , b , c }.contains( e ) || triangle{ a , b , c }.contains( o ) )
+			if( triangle{ a , b , c }.contains( end ) || triangle{ a , b , c }.contains( origin ) )
 			{
 				return true;
 			}
 			//degenerate cases
-			if( o == e )//o->e is point
+			if( origin == end )//o->e is point
 			{
 				if( edge{ a , b }.intersects( c ) )//triangle is line a->b
 				{
 					if( b == a )//a->b is point
 					{
-						return a == o;
+						return a == origin;
 					} else
 					{
-						return edge{ a , b }.intersects( o );
+						return edge{ a , b }.intersects( origin );
 					}
 				} else if( edge{ a , c }.intersects( b ) )//triangle is line a->c
 				{
-					return edge{ a , c }.intersects( o );
+					return edge{ a , c }.intersects( origin );
 				} else if( edge{ b , c }.intersects( a ) )//triangle is line b->c
 				{
-					return edge{ b , c }.intersects( o );
+					return edge{ b , c }.intersects( origin );
 				} else
 				{
-					return triangle{ a , b , c }.contains( o );
+					return triangle{ a , b , c }.contains( origin );
 				}
 			}
 			if( edge{ a , b }.intersects( c ) )//triangle is line a->b
@@ -222,23 +222,28 @@ namespace SchreinerA
 					return intersects( a );
 				} else
 				{
-					return edge{ a , b }.intersects( o , e );
+					return edge{ a , b }.intersects( origin , end );
 				}
 			} else if( edge{ a , c }.intersects( b ) )//triangle is line a->c
 			{
-				return edge{ a , c }.intersects( o , e );
+				return edge{ a , c }.intersects( origin , end );
 			} else if( edge{ b , c }.intersects( a ) )//triangle is line b->c
 			{
-				return edge{ b , c }.intersects( o , e );
+				return edge{ b , c }.intersects( origin , end );
 			} else
 			{
-				vec3 dir = ( e - o ).norm();
 				plane p = { a , ( ( b - a ) ^ ( c - a ) ).norm() };
-				vec3 proj = p.project( o , e );
+				if( abs( p.normal * ( origin - end ).norm() ) < EPS )
+				{
+					return intersects( a , b ) ||
+						intersects( b , c ) ||
+						intersects( c , a ) ||
+						triangle{ a , b , c }.contains( origin ) ||
+						triangle{ a , b , c }.contains( end );
+				}
+				vec3 proj = p.project( origin , end );
 				return triangle{ a , b , c }.contains( proj );
 			}
-
-			
 		}
 	};
 	bool intersects( double *a , double *b )
@@ -272,63 +277,4 @@ namespace SchreinerA
 		}
 		return false;
 	}
-}
-#include <iostream>
-int main( int argc , char **argv )
-{
-	using namespace SchreinerA;
-	double triangles[] =
-	{
-		0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 ,
-		//0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , nan( "" ) ,
-		//0.0 , 0.0 , 0.0 , std::numeric_limits<double>::infinity() , 0.0 , 0.0 , 0.0 , 0.0 , nan( "" ) ,
-		//0.0 , 0.0 , 0.0 , 1.0 , 0.0 , 0.0 , 0.0 , 1.0 , 0.0 ,
-		//0.5 , 0.0 , 0.0 , 0.0 , 0.5 , 0.0 , 1.0 , 1.0 , 0.0 ,
-		//1.0 , 0.0 , 0.0 , 0.0 , 1.0 , 0.0 , 1.0 , 1.0 , 1.0 ,
-		//1.0 , 0.0 , 1.0 , 0.0 , 1.0 , 1.0 , 0.0 , 0.0 , -1.0 ,
-		//0.0 , 0.0 , 1.0 , 0.0 , 1.0 , -1.0 , 1.0 , 0.0 , -1.0 ,
-		0.5 , -1.0 , 0.5 , 0.5 , 1.0 , 0.5 , 0.5 , 0.0 , 1.5 ,
-		0.0 , 0.0 , 0.0 , 0.5 , 0.0 , 0.5 , 2.0 , 0.0 , 2.0 ,
-		
-	};
-	double dphi = acos( -1.0 ) / 32;
-	for( int k = 0; k < 32; k++ )
-	{
-		std::cout << "_______" << k * dphi << std::endl;
-		for( int i = 0; i < sizeof( triangles ) / 9 / 8; i++ )
-		{
-			for( int j = 0; j < sizeof( triangles ) / 9 / 8; j++ )
-			{
-				std::cout << intersects( triangles + i * 9 , triangles + j * 9 ) << " ";
-			}
-			std::cout << std::endl;
-		}
-		for( int i = 0; i < sizeof( triangles ) / 3 / 8; i++ )
-		{
-			vec3 &p = *( vec3* )( triangles + i * 3 );
-			vec3 c = p;
-			p.y = c.y * cos( dphi ) + -c.z * sin( dphi );
-			p.z = c.y * sin( dphi ) + c.z * cos( dphi );
-		}
-	}
-	std::cout << edge{ { 0.0 , 0.0 , 0.0 } , { 2.0 , 0.0 , 0.0 } }.intersects(
-			{ 1.0 , 1.0 , 0.0 } , { 1.0 , 0.01 , 0.0 }
-		) << "=0" << std::endl;
-	std::cout << edge{ { 0.0 , 0.0 , 0.0 } ,{ 2.0 , 0.0 , 0.0 } }.intersects(
-	{ 1.0 , 1.0 , 0.0 } , { 1.0 , 0.0 , 0.0 }
-	) << "=1" << std::endl;
-	std::cout << edge{ { 0.0 , 0.0 , 0.0 } ,{ 0.0 , 2.0 , 0.0 } }.intersects(
-	{ 0.0 , 1.0 , 0.0 } , { 0.0 , 1.0 , 1.0 }
-	) << "=1" << std::endl;
-	std::cout << edge{ { 0.0 , 0.0 , 0.0 } ,{ 0.0 , 2.0 , 0.0 } }.intersects(
-	{ 0.0 , 1.0 , -1.0 } , { 0.0 , 1.0 , 1.0 }
-	) << "=1" << std::endl;
-	std::cout << edge{ { 0.0 , 0.0 , 0.0 } ,{ 0.0 , 2.0 , 0.0 } }.intersects(
-	{ 0.0 , 2.0 , -1.0 } , { 0.0 , 1.0 , 1.0 }
-	) << "=1" << std::endl;
-	std::cout << edge{ { 0.0 , 0.0 , 0.0 } ,{ 0.0 , 2.0 , 0.0 } }.intersects(
-	{ 0.0 , 2.0 , -1.0 } , { 0.0 , 1.0 , -1.0 }
-	) << "=0" << std::endl;
-	getchar();
-	return 0;
 }
